9b60bde5e2acc239265d3f09e8664c71ddda70fb
Sean McEmerson <boredofmath+github@gmail.com>
2024-10-10 00:11:09 +0100

    chore: Remove old config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,28 +8,6 @@ module.exports = {
     plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
     theme: {
         extend: {
-            colors: {
-                'bg-color': 'var(--theme-beige-50)',
-                'text-color': 'var(--theme-black)',
-                'accent-color': 'var(--theme-beige-100)',
-                'button-bg-color': 'var(--theme-beige-100)',
-                'button-text-color': 'var(--theme-black)',
-                'button-bg-color-hover': 'var(--theme-beige-300)',
-                'photo-border-color': 'var(--theme-black)',
-                'link-text-color': 'var(--theme-beige-black)',
-                'link-text-color-hover': 'var(--theme-black)',
-                'overlay-bg-color': 'var(--theme-black)',
-                'dark-bg-color': 'var(--theme-beige-950)',
-                'dark-text-color': 'var(--theme-beige-50)',
-                'dark-accent-color': 'var(--theme-beige-950)',
-                'dark-button-bg-color': 'var(--theme-beige-950)',
-                'dark-button-text-color': 'var(--theme-beige-50)',
-                'dark-button-bg-color-hover': 'var(--theme-beige-900)',
-                'dark-photo-border-color': 'var(--theme-beige-50)',
-                'dark-link-text-color': 'var(--theme-beige-50)',
-                'dark-link-text-color-hover': 'var(--theme-beige-100)',
-                'dark-overlay-bg-color': 'var(--theme-beige-50)'
-            },
             fontFamily: {
                 sans: ['var(--font-geist-sans)']
             }
