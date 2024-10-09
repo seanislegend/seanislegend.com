8ae5f87b65fc87fbf4b85505e800e130bf184dfc
Sean McEmerson <boredofmath+github@gmail.com>
2024-10-09 23:19:36 +0100

    feat: Add hover label for colletion link

--- src/components/SiteHeader/PageHeaderTitlePreview.tsx
+++ src/components/SiteHeader/PageHeaderTitlePreview.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import {useAtomValue} from 'jotai';
 import Link from 'next/link';
+import {LeftArrowIcon} from '@/components/Icon/LeftArrow';
 import Condition from '@/components/UI/Condition';
 import useScrollStatus from '@/hooks/useScrollStatus';
 import {pageHeaderDataAtom} from '@/utils/store';
@@ -24,10 +25,16 @@ const PageHeaderTitlePreview: React.FC = () => {
                 condition={pageHeaderData.path}
                 wrapper={children => (
                     <Link
-                        className="inline-block truncate underline-offset-4 hover:underline"
+                        className="group relative inline-block truncate underline-offset-4 hover:underline"
                         href={pageHeaderData.path}
                     >
-                        {children}
+                        <span className="absolute bottom-0 left-0 top-0 flex translate-x-2 items-center gap-1 opacity-0 duration-300 ease-in-out group-hover:translate-x-0 group-hover:opacity-100">
+                            <LeftArrowIcon className="h-5 w-5 fill-current" />
+                            <span>Go to collection</span>
+                        </span>
+                        <span className="duration-500 ease-in-out group-hover:translate-x-1 group-hover:opacity-0 group-hover:duration-200">
+                            {children}
+                        </span>
                     </Link>
                 )}
             >
