d3f48ccb6e44d538825bcf31c8e169646bdbaedc
Sean McEmerson <boredofmath+github@gmail.com>
2024-12-11 07:56:17 +0000

    chore: update prettier extension

--- src/components/PhotoCollection/Thumbnail.tsx
+++ src/components/PhotoCollection/Thumbnail.tsx
@@ -4,20 +4,20 @@ import {RightArrowIcon} from '@/components/Icon';
 import Condition from '@/components/UI/Condition';
 import ThumbnailImage from './ThumbnailImage';
 
-type ThumbnailPhoto = Pick<Photo, 'base64' | 'slug' | 'thumbnail' | 'title'>;
+type ThumbnailPhoto=Pick<Photo,'base64'|'slug'|'thumbnail'|'title'>;
 interface Props extends ThumbnailPhoto {
     label?: string;
-    loading?: 'eager' | 'lazy';
-    linksTo?: 'collection' | 'photo';
+    loading?: 'eager'|'lazy';
+    linksTo?: 'collection'|'photo';
     path: string;
     [key: string]: any;
 }
 
-const PhotoThumbnail: React.FC<Props> = ({
+const PhotoThumbnail: React.FC<Props>=({
     base64,
     label,
     linksTo,
-    loading = 'lazy',
+    loading='lazy',
     path,
     slug,
     thumbnail,
@@ -43,19 +43,19 @@ const PhotoThumbnail: React.FC<Props> = ({
                     className={clsx(
                         'absolute inset-0 z-30 w-full opacity-0 transition-opacity duration-500 ease-in-out group-hover/photo:opacity-100 group-hover/photo:duration-200',
                         {
-                            'bg-[var(--photo-bg)]': linksTo !== 'collection',
-                            'bg-[var(--photo-bg-badge)]': linksTo === 'collection'
+                            'bg-[var(--photo-bg)]': linksTo!=='collection',
+                            'bg-[var(--photo-bg-badge)]': linksTo==='collection'
                         }
                     )}
                 />
-                {linksTo === 'collection' && (
-                    <span className="absolute bottom-2 right-2 hidden h-full w-full overflow-hidden sm:block">
-                        <span className="absolute bottom-0 right-[2.5rem] z-40 h-[2.25rem] translate-x-full bg-[var(--button-bg-hover)] px-4 py-2 pr-0 text-sm font-medium uppercase text-[var(--button-text)] transition duration-300 ease-in-out group-hover/photo:translate-x-0 group-hover/photo:duration-200">
+                {linksTo==='collection'&&(
+                    <span className="absolute bottom-2 right-2 h-full w-full overflow-hidden">
+                        <span className="absolute bottom-0 right-[2.5rem] z-40 h-[2.25rem] translate-x-full bg-[var(--button-bg-hover)] px-4 py-2 pr-0 text-sm font-medium uppercase text-[var(--button-text)] transition duration-300 ease-in-out group-hover/photo:translate-x-0 group-hover/photo:duration-200 min-[1320px]:block hidden">
                             <span className="opacity-0 transition-opacity duration-200 group-hover/photo:opacity-100">
-                                View {linksTo === 'collection' ? 'collection' : 'photo'}
+                                View {linksTo==='collection'? 'collection':'photo'}
                             </span>
                         </span>
-                        <span className="absolute bottom-0 right-0 z-40 h-[2.25rem] w-[2.5rem] bg-[var(--button-bg-hover)] px-2 py-2 text-[var(--button-text)]">
+                        <span className="absolute bottom-0 right-0 z-40 h-7 w-7 sm:h-[2.25rem] sm:w-[2.5rem] bg-[var(--button-bg-hover)] p-1 sm:p-2 text-[var(--button-text)]">
                             <RightArrowIcon className="absolute h-5 w-5 fill-current" />
                         </span>
                     </span>
