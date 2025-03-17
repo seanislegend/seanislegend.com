b631cce655640c7d47917b53680280e74d4135a6
Sean McEmerson <boredofmath+github@gmail.com>
2025-03-17 22:27:53 +0000

    chore: Update copy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,12 @@ import PageHeader from '@/components/PageHeader';
 const NotFound = () => (
     <DefaultLayout theme="light">
         <PageHeader title="Not found">
-            <p>Could not find requested resource</p>
-            <Button className="mt-4" href="/">
-                Go to home
+            <p>
+                Sorry, the page you are looking for does not exist. If you&apos;re looking for a
+                photo or collection, look through the links below.
+            </p>
+            <Button className="mt-4" href="/collections">
+                View all collections
             </Button>
         </PageHeader>
     </DefaultLayout>
