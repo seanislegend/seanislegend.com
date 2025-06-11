dbe6a2ee872efbd43f48f85b2160bd25e8ae47b9
Sean McEmerson <boredofmath+github@gmail.com>
2025-06-12 00:56:52 +0100

    feat: Add reusable headings

---
+++ src/components/UI/Headings.tsx
@@ -0,0 +1,11 @@
+export const Heading1: React.FC<React.PropsWithChildren> = ({children}) => (
+    <h1 className="text-title-text sticky top-18 max-w-5xl space-x-2 text-2xl leading-tight font-medium text-balance break-normal uppercase underline-offset-4 group-hover:underline md:text-3xl md:leading-tight lg:text-4xl">
+        <span>{children}</span>
+    </h1>
+);
+
+export const Heading2: React.FC<React.PropsWithChildren> = ({children}) => (
+    <h2 className="text-title-text sticky top-18 max-w-5xl space-x-2 text-lg leading-tight font-medium text-balance break-normal uppercase underline-offset-4 group-hover:underline md:text-xl md:leading-tight lg:text-2xl">
+        {children}
+    </h2>
+);
