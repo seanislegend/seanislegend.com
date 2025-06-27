f8c5ee1e750850aabfae97f6bbf6d17d85c35f92
Sean McEmerson <boredofmath+github@gmail.com>
2025-06-27 15:47:42 +0100

    feat: Add secondary theme for buttons

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,6 +6,7 @@ import {isExternalUrl} from '@/utils/helpers';
 interface Props {
     className?: string;
     href?: Url | string | undefined;
+    theme?: 'primary' | 'secondary';
     [key: string]: any;
 }
 
@@ -13,12 +14,19 @@ const Button: React.FC<React.PropsWithChildren<Props>> = ({
     children,
     className,
     href,
+    theme = 'primary',
     ...props
 }) => {
     const Comp = href ? Link : 'button';
     const classes = clsx([
-        'bg-button-bg text-button-text hover:bg-button-bg-hover focus:ring-button-bg-hover inline-block rounded-xs px-3 py-1.5 text-[13px] font-medium tracking-wider uppercase transition duration-200 ease-out focus:ring-2 focus:ring-offset-2 focus:outline-hidden sm:px-4 sm:py-2 sm:text-sm',
-        className
+        'inline-block rounded-xs px-3 py-1.5 text-[13px] font-medium tracking-wider uppercase transition duration-200 ease-out focus:ring-2 focus:ring-offset-2 focus:outline-hidden sm:px-4 sm:py-2 sm:text-sm',
+        className,
+        {
+            'bg-button-bg text-button-text hover:bg-button-bg-hover focus:ring-button-bg-hover':
+                theme === 'primary',
+            'bg-secondary-button-bg text-secondary-button-text hover:bg-secondary-button-bg-hover':
+                theme === 'secondary'
+        }
     ]);
 
     if (isExternalUrl(href as string)) {
