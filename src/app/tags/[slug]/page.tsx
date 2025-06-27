87be92ea2d7325060c241612428b487bf796116a
Sean McEmerson <boredofmath+github@gmail.com>
2025-06-27 15:57:51 +0100

    feat: Add more tags list

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -2,6 +2,9 @@ import {notFound} from 'next/navigation';
 import DefaultLayout from '@/components/Layouts/Default';
 import PageHeader from '@/components/PageHeader';
 import PhotoMasonry from '@/components/PhotoCollection/Masonry';
+import Container from '@/components/UI/Container';
+import {Heading2} from '@/components/UI/Headings';
+import TextLink from '@/components/UI/TextLink';
 import config from '@/utils/config';
 import {fetchAllPhotosForTag, fetchAllTags} from '@/utils/contentful';
 import {getTagSeo} from '@/utils/helpers';
@@ -12,6 +15,7 @@ interface Props {
 
 const TagDetailPage = async ({params}: Props) => {
     const allParams = await params;
+    const allTags = await fetchAllTags();
     const {tag, photos} = await fetchAllPhotosForTag(allParams.slug);
 
     if (!tag || !photos?.length) {
@@ -29,6 +33,20 @@ const TagDetailPage = async ({params}: Props) => {
             />
 
             <PhotoMasonry items={photos} />
+            {allTags?.length > 0 && (
+                <Container className="my-10 lg:my-20">
+                    <Heading2>More tags</Heading2>
+                    <ul className="mt-4 flex flex-row flex-wrap gap-x-4 gap-y-2">
+                        {allTags
+                            .filter((t: Tag) => t.slug !== tag.slug)
+                            .map((t: Tag) => (
+                                <li key={t.slug}>
+                                    <TextLink href={`/tags/${t.slug}`}>{t.name}</TextLink>
+                                </li>
+                            ))}
+                    </ul>
+                </Container>
+            )}
         </DefaultLayout>
     );
 };
