14666ea51f25ca3bb58e5ea129df67b9d64d4b69
Sean McEmerson <boredofmath+github@gmail.com>
2025-06-28 14:00:14 +0100

    chore: Remove sticky headers

--- src/components/UI/Headings.tsx
+++ src/components/UI/Headings.tsx
@@ -1,5 +1,5 @@
 export const Heading1: React.FC<React.PropsWithChildren> = ({children}) => (
-    <h1 className="text-title-text sticky top-18 max-w-5xl space-x-2 text-2xl leading-tight font-medium text-balance break-normal uppercase underline-offset-4 group-hover:underline md:text-3xl md:leading-tight lg:text-4xl">
+    <h1 className="text-title-text max-w-5xl space-x-2 text-2xl leading-tight font-medium text-balance break-normal uppercase underline-offset-4 group-hover:underline md:text-3xl md:leading-tight lg:text-4xl">
         <span>{children}</span>
     </h1>
 );
