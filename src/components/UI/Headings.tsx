27a0112aacfaad7a00ad7022c455549a661f2137
Sean McEmerson <boredofmath+github@gmail.com>
2025-06-28 13:03:12 +0100

    chore: Type tweaks

--- src/components/UI/Headings.tsx
+++ src/components/UI/Headings.tsx
@@ -5,7 +5,7 @@ export const Heading1: React.FC<React.PropsWithChildren> = ({children}) => (
 );
 
 export const Heading2: React.FC<React.PropsWithChildren> = ({children}) => (
-    <h2 className="text-title-text sticky top-18 max-w-5xl space-x-2 text-lg leading-tight font-medium text-balance break-normal uppercase underline-offset-4 group-hover:underline md:text-xl md:leading-tight lg:text-2xl">
+    <h2 className="text-title-text sticky top-18 max-w-5xl space-x-2 text-xl leading-tight font-medium text-balance break-normal uppercase underline-offset-4 group-hover:underline md:leading-tight lg:text-2xl">
         {children}
     </h2>
 );
