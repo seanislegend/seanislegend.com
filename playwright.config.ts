689a1f55f4f7e6d6b3f7feb9cb5876d8c20c60fa
Sean McEmerson <boredofmath+github@gmail.com>
2025-07-15 00:38:30 +0100

    feat: Update tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
         reuseExistingServer: !process.env.CI,
         env: {
             NODE_ENV: 'test',
+            NEXT_PUBLIC_URL: 'http://localhost:3000',
             CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID!,
             CONTENTFUL_DELIVERY_API_KEY: process.env.CONTENTFUL_DELIVERY_API_KEY!,
             CONTENTFUL_PREVIEW_API_KEY: process.env.CONTENTFUL_PREVIEW_API_KEY!
