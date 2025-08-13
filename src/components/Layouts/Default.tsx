96c958462c406a309ad9c14401c8a6c270114081
Sean McEmerson <boredofmath+github@gmail.com>
2025-08-13 14:57:30 +0100

    fix: Remove Radix props

--- src/components/Layouts/Default.tsx
+++ src/components/Layouts/Default.tsx
@@ -7,9 +7,9 @@ interface Props {
 }
 
 const DefaultLayout: React.FC<React.PropsWithChildren<Props>> = ({children, theme}) => (
-    <Theme className="flex grow flex-col sm:min-h-full" theme={theme}>
+    <Theme className="default-layout flex grow flex-col sm:min-h-full" theme={theme}>
         <SiteHeader />
-        <main className="grow" data-testid="page-content">
+        <main className="grow" data-testid="page-content" data-layout-main="default">
             {children}
         </main>
         <SiteFooter />
