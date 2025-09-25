23e07ad11cff49b82fc745bb85ec32d7d68d99e2
Sean McEmerson <boredofmath+github@gmail.com>
2025-09-25 14:10:02 +0100

    feat: Add path revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,6 +1,114 @@
+import {revalidatePath} from 'next/cache';
 import {NextRequest} from 'next/server';
+import {fetchContent} from '@/utils/contentful';
+
+const LOCALE = process.env.CONTENTFUL_LOCALE ?? '';
+
+const revalidate = (path: string) => {
+    if (process.env.NODE_ENV === 'development') {
+        console.log(`Revalidating: ${path}`);
+    }
+    revalidatePath(path);
+};
+
+const revalidatePathForType = async (body: any) => {
+    const type = body?.sys?.contentType?.sys?.id ?? body?.sys?.type;
+    console.log('type', type);
+    if (!type) return;
+
+    if (type === 'collection') {
+        const slug = body?.fields?.slug?.[LOCALE];
+        if (slug) {
+            revalidate(`/${slug}`);
+        }
+    } else if (type === 'photo') {
+        const {data} = await fetchContent(`query {
+            photo(id: "${body.sys.id}") {
+                tagsCollection {
+                    items {
+                        slug
+                    }
+                }
+                linkedFrom {
+                    collectionCollection {
+                        items {
+                            slug
+                        }
+                    }
+                    photoGridPhotoCollection {
+                        items {
+                            _id
+                        }
+                    }
+                }
+            }
+        }`);
+        if (!data?.photo) return;
+
+        data.photo.tagsCollection.items.forEach((tag: any) => {
+            revalidate(`/${tag.slug}-photography`);
+        });
+        data.photo.linkedFrom.collectionCollection.items.forEach((collection: any) => {
+            revalidate(`/${collection.slug}`);
+        });
+        // if used in a photo grid, revalidate the services page as this is the
+        // only use case for this
+        if (data.photo.linkedFrom.photoGridPhotoCollection.items.length > 0) {
+            revalidate(`/services`);
+        }
+    } else if (type === 'Asset') {
+        const {data} = await fetchContent(`query {
+            asset(id:"${body.sys.id}"){
+                linkedFrom{
+                    photoCollection {
+                        items {
+                            slug
+                            linkedFrom {
+                                collectionCollection {
+                                    items {
+                                        slug
+                                    }
+                                }
+                            }
+                        }
+                    }   
+                    editorialCollection {
+                        items {
+                            slug
+                        }
+                    }
+                }
+            }
+        }`);
+        if (!data?.asset) return;
+
+        data.asset.linkedFrom.editorialCollection.items.forEach((page: any) => {
+            revalidate(`/${page.slug}`);
+        });
+        data.asset.linkedFrom.photoCollection.items.forEach((photo: any) => {
+            // for each photo we need to revalidate the collection it belongs to
+            photo.linkedFrom.collectionCollection.items.forEach((collection: any) => {
+                revalidate(`/${collection.slug}`);
+                // home page does not nest photos in a collection
+                if (collection.slug !== 'home') {
+                    revalidate(`/${collection.slug}/${photo.slug}`);
+                }
+            });
+        });
+    }
+};
 
 export const POST = async (request: NextRequest) => {
-    console.log(request);
+    const secret = request.headers.get('X-SIL-Secret');
+
+    if (!secret || secret !== process.env.REVALIDATE_SECRET) {
+        return Response.json({message: 'Invalid secret', success: false});
+    } else if (!request.body) {
+        return Response.json({message: 'No body', success: false});
+    }
+
+    const body = await request.json();
+    await revalidatePathForType(body);
+
     return Response.json({success: true});
 };
