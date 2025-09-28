dfe58e40465d2c267e3ffdc512db872304fdbcfc
Sean McEmerson <boredofmath+github@gmail.com>
2025-09-28 21:24:46 +0100

    feat: Support multiple layouts for og image

--- src/utils/og.tsx
+++ src/utils/og.tsx
@@ -1,42 +1,105 @@
 import {ImageResponse} from '@vercel/og';
 
-export const getOgImage = async (photos: string[]) => {
+const ImageWrapper = ({children}: {children: React.ReactNode}) => {
+    return (
+        <div tw="relative flex w-full flex-col items-center justify-center bg-white">
+            <div tw="flex w-full bg-white h-full flex-wrap">{children}</div>
+            <div
+                style={{
+                    backgroundImage: `url(${process.env.NEXT_PUBLIC_URL}/images/og-logo.png)`,
+                    backgroundSize: '100% 100%'
+                }}
+                tw="absolute bottom-2 right-2 h-10 w-10"
+            />
+        </div>
+    );
+};
+
+const generatedOneImage = async (photo: string) => {
     return new ImageResponse(
         (
-            <div tw="relative flex w-full flex-col items-center justify-center bg-white">
-                <div tw="flex w-full bg-white h-full flex-wrap">
-                    {photos.map((photo, index) => {
-                        const className =
-                            photos.length > 1
-                                ? `flex w-full bg-gray-100 w-1/2 h-1/2 bg-cover bg-center bg-no-repeat ${
-                                      index % 2 === 0 ? 'border-r-4 border-white' : ''
-                                  } ${index < 2 ? 'border-b-4 border-white' : ''}`
-                                : 'bg-no-repeat bg-cover bg-center bg-no-repeat w-full h-full absolute';
-
-                        return (
-                            <div
-                                key={photo}
-                                tw={className}
-                                style={{
-                                    backgroundImage: `url(${photo
-                                        .replace('fm=webp', '')
-                                        .replace('w=1800', 'w=800')})`,
-                                    backgroundSize: '100% 130%',
-                                    backgroundPosition: '0 -15%'
-                                }}
-                            />
-                        );
-                    })}
-                </div>
+            <ImageWrapper>
                 <div
+                    key={photo}
+                    tw="w-full h-full absolute"
                     style={{
-                        backgroundImage: `url(${process.env.NEXT_PUBLIC_URL}/images/og-logo.png)`,
-                        backgroundSize: '100% 100%'
+                        backgroundImage: `url(${photo
+                            .replace('fm=webp', '')
+                            .replace('w=1800', 'w=800')})`,
+                        backgroundSize: '100% 100%',
+                        backgroundPosition: '0 center'
                     }}
-                    tw="absolute bottom-2 right-2 h-10 w-10"
                 />
-            </div>
+            </ImageWrapper>
         ),
         {width: 1200, height: 630}
     );
+};
+
+const generatedFourTiledImage = async (photos: string[]) => {
+    return new ImageResponse(
+        (
+            <ImageWrapper>
+                {photos.map((photo, index) => {
+                    const className = `flex bg-gray-100 w-1/2 h-1/2 ${
+                        index % 2 === 0 ? 'border-r-4 border-white' : ''
+                    } ${index < 2 ? 'border-b-4 border-white' : ''}`;
+
+                    return (
+                        <div
+                            key={photo}
+                            tw={className}
+                            style={{
+                                backgroundImage: `url(${photo
+                                    .replace('fm=webp', '')
+                                    .replace('w=1800', 'w=800')})`,
+                                backgroundSize: '100% 134%',
+                                backgroundPosition: '0 -17%'
+                            }}
+                        />
+                    );
+                })}
+            </ImageWrapper>
+        ),
+        {width: 1200, height: 630}
+    );
+};
+
+const generatedTwoTiledImage = async (photos: string[]) => {
+    return new ImageResponse(
+        (
+            <ImageWrapper>
+                {photos.map((photo, index) => {
+                    const className = `flex bg-gray-100 w-1/2 h-full ${
+                        index % 2 === 0 ? 'border-r-4 border-white' : ''
+                    }`;
+
+                    return (
+                        <div
+                            key={photo}
+                            tw={className}
+                            style={{
+                                backgroundImage: `url(${photo
+                                    .replace('fm=webp', '')
+                                    .replace('w=1800', 'w=800')})`,
+                                backgroundSize: '134% 100%',
+                                backgroundPosition: '-17% 0'
+                            }}
+                        />
+                    );
+                })}
+            </ImageWrapper>
+        ),
+        {width: 1200, height: 630}
+    );
+};
+
+export const getOgImage = async (photos: string[]) => {
+    if (photos.length === 1) {
+        return generatedOneImage(photos[0]);
+    } else if (photos.length === 2) {
+        return generatedTwoTiledImage(photos);
+    }
+
+    return generatedFourTiledImage(photos);
 };
