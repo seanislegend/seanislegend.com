457b35df41c0c7a17ae549401d3eac6e1980153b
Sean McEmerson <boredofmath+github@gmail.com>
2025-09-30 00:29:05 +0100

    feat: Add resuable content sections

---
+++ src/app/services/ContentSection.tsx
@@ -0,0 +1,69 @@
+import Markdown from '@/components/Markdown';
+import PageHeaderButtonList from '@/components/PageHeader/ButtonList';
+import {Heading2} from '@/components/UI/Headings';
+import TextLink from '@/components/UI/TextLink';
+import TitleTextGrid from '@/components/UI/TitleTextGrid';
+import PhotosGrid from '@/app/services/PhotosGrid';
+
+interface Props {
+    ctaStyle: 'button' | 'link';
+    sections: ContentSection[];
+}
+
+const ContentSections: React.FC<Props> = ({ctaStyle, sections}) => (
+    <>
+        {sections.map((section: ContentSection) => {
+            const ctas: {label: string; url: string}[] = [];
+
+            if (ctaStyle === 'button') {
+                if (sections[0].ctaLabel && sections[0].ctaUrl) {
+                    ctas.push({label: sections[0].ctaLabel, url: sections[0].ctaUrl});
+                }
+                if (sections[0].secondaryCtaLabel && sections[0].secondaryCtaUrl) {
+                    ctas.push({
+                        label: sections[0].secondaryCtaLabel,
+                        url: sections[0].secondaryCtaUrl
+                    });
+                }
+            }
+
+            return (
+                <div key={section.title}>
+                    {section.photoGrid?.photosCollection?.items && (
+                        <PhotosGrid photos={section.photoGrid.photosCollection.items} />
+                    )}
+                    <TitleTextGrid className="mt-8" heading={<Heading2>{section.title}</Heading2>}>
+                        <Markdown className="max-w-2xl text-pretty 2xl:max-w-5xl">
+                            {section.content}
+                        </Markdown>
+                        {ctaStyle === 'button' && (
+                            <div className="mt-4">
+                                <PageHeaderButtonList ctas={ctas} />
+                            </div>
+                        )}
+                        {ctaStyle === 'link' && (
+                            <div className="mt-4">
+                                <TextLink
+                                    href={`${section?.ctaUrl ?? '/contact'}?service=${section.title}`}
+                                >
+                                    {section?.ctaLabel ?? 'Enquire'} about{' '}
+                                    {section?.title?.toLowerCase()}
+                                </TextLink>{' '}
+                                {section.id && (
+                                    <span>
+                                        or{' '}
+                                        <TextLink href={`/${section.id}-photography`}>
+                                            View more photos
+                                        </TextLink>
+                                    </span>
+                                )}
+                            </div>
+                        )}
+                    </TitleTextGrid>
+                </div>
+            );
+        })}
+    </>
+);
+
+export default ContentSections;
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,15 +1,11 @@
 import type {Metadata} from 'next';
 import DefaultLayout from '@/components/Layouts/Default';
-import Markdown from '@/components/Markdown';
 import PageHeader from '@/components/PageHeader';
 import Container from '@/components/UI/Container';
-import {Heading2} from '@/components/UI/Headings';
-import TextLink from '@/components/UI/TextLink';
-import TitleTextGrid from '@/components/UI/TitleTextGrid';
 import config from '@/utils/config';
 import {fetchEditorialPage} from '@/utils/contentful';
 import {getEditorialSeo} from '@/utils/helpers';
-import PhotosGrid from '@/app/services/PhotosGrid';
+import ContentSections from '@/app/services/ContentSection';
 
 const ServicesPage = async () => {
     const page = await fetchEditorialPage('services');
@@ -18,37 +14,7 @@ const ServicesPage = async () => {
         <DefaultLayout theme="light">
             <PageHeader description={page.content} title={page.pageTitle} />
             <Container className="space-y-16 lg:space-y-24">
-                {page.contentSectionsCollection.items.map((section: ContentSection) => (
-                    <div key={section.title}>
-                        {section.photoGrid?.photosCollection?.items && (
-                            <PhotosGrid photos={section.photoGrid.photosCollection.items} />
-                        )}
-                        <TitleTextGrid
-                            className="mt-8"
-                            heading={<Heading2>{section.title}</Heading2>}
-                        >
-                            <Markdown className="max-w-2xl text-pretty 2xl:max-w-5xl">
-                                {section.content}
-                            </Markdown>
-                            <p className="mt-4">
-                                <TextLink
-                                    href={`${section?.ctaUrl ?? '/contact'}?service=${section.title}`}
-                                >
-                                    {section?.ctaLabel ?? 'Enquire'} about{' '}
-                                    {section?.title?.toLowerCase()}
-                                </TextLink>{' '}
-                                {section.id && (
-                                    <span>
-                                        or{' '}
-                                        <TextLink href={`/${section.id}-photography`}>
-                                            View more photos
-                                        </TextLink>
-                                    </span>
-                                )}
-                            </p>
-                        </TitleTextGrid>
-                    </div>
-                ))}
+                <ContentSections sections={page.contentSectionsCollection.items} />
             </Container>
         </DefaultLayout>
     );
