cac682f7c07be5e42a9b527fc8a77099c81fea03
Sean McEmerson <boredofmath+github@gmail.com>
2025-10-24 16:42:03 +0100

    feat: Add styleable collection layouts

--- src/components/Layouts/Default.tsx
+++ src/components/Layouts/Default.tsx
@@ -2,12 +2,12 @@ import SiteFooter from '@/components/SiteFooter';
 import SiteHeader from '@/components/SiteHeader';
 import Theme from '@/components/UI/Theme';
 
-interface Props {
+interface Props extends React.ComponentProps<'div'> {
     theme: ThemeType;
 }
 
-const DefaultLayout: React.FC<React.PropsWithChildren<Props>> = ({children, theme}) => (
-    <Theme className="default-layout flex grow flex-col sm:min-h-full" theme={theme}>
+const DefaultLayout: React.FC<Props> = ({children, theme, ...props}) => (
+    <Theme className="default-layout flex grow flex-col sm:min-h-full" theme={theme} {...props}>
         <SiteHeader />
         <main className="grow" data-testid="page-content" data-layout-main="default">
             {children}
