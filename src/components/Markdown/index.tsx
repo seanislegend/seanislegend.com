2899169e1abae6ceee386deba61b46ae79a7d454
Sean McEmerson <boredofmath+github@gmail.com>
2025-10-24 18:18:29 +0100

    feat: Style quotes

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 export const BASE_TEXT_CLASS =
-    'dark:prose-invert prose-p:tracking-[.0185rem] md:prose-p:text-base md:prose-p:leading-normal 2xl:prose-p:text-[17px] text-sm leading-relaxed';
+    'dark:prose-invert prose-p:tracking-[.0185rem] md:prose-p:text-base md:prose-p:leading-normal 2xl:prose-p:text-[17px] text-sm leading-relaxed prose-blockquote:text-lg @2xl:prose-blockquote:text-xl';
 
 const MarkdownLink = (props: any) => (
     <a
@@ -32,7 +32,37 @@ const Markdown: React.FC<Props> = ({allowLinks = true, children, className = ''}
                 ul: ({...props}) => (
                     <ul {...props} className="list-inside pl-4 text-base 2xl:text-[17px]" />
                 ),
-                li: ({...props}) => <li {...props} className="list-disc" />
+                li: ({...props}) => <li {...props} className="list-disc" />,
+                blockquote: ({...props}) => {
+                    // to get the quote text we need to find it in contentful's blockquote format
+                    const element = props.node?.children.find(
+                        (child: any) => child.type === 'element'
+                    );
+                    if (!element) return null;
+
+                    const item =
+                        'children' in element
+                            ? element.children?.find(
+                                  (child: any) => child.type === 'text' && child.value
+                              )
+                            : null;
+
+                    // @ts-ignore
+                    const parts = item.value.split('|');
+                    if (parts.length !== 2) return null;
+
+                    const quote = parts[0];
+                    const author = parts[1];
+
+                    return (
+                        <figure className="border-accent border-l-4 pl-4">
+                            <blockquote>
+                                <p className="!text-lg italic lg:!text-xl xl:!text-2xl">{quote}</p>
+                            </blockquote>
+                            <figcaption>— {author}</figcaption>
+                        </figure>
+                    );
+                }
             }}
         >
             {children}
