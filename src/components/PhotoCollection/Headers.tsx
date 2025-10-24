159376af70a4b41f3add25825085de4a591aca37
Sean McEmerson <boredofmath+github@gmail.com>
2025-10-24 02:38:16 +0100

    feat: Editorial layout styling

--- src/components/PhotoCollection/Headers.tsx
+++ src/components/PhotoCollection/Headers.tsx
@@ -32,7 +32,7 @@ const GreenHopHeader: React.FC<React.PropsWithChildren<Props>> = ({collection}) 
     const title2 = collection.pageTitle?.split(':')[1];
 
     return (
-        <Container className="animate-in fade-in slide-in-from-bottom-2 pt-6 pb-10 duration-500 sm:py-12 xl:py-20">
+        <Container className="animate-in fade-in slide-in-from-bottom-2 -mb-24 pt-6 pb-10 duration-500 sm:py-12 xl:py-20">
             <h1 className="text-title-text overflow-hidden leading-none uppercase underline-offset-4 group-hover:underline sm:whitespace-nowrap">
                 <span
                     className="block font-extrabold tracking-tight text-pretty 2xl:!text-[7.3rem]"
@@ -47,12 +47,11 @@ const GreenHopHeader: React.FC<React.PropsWithChildren<Props>> = ({collection}) 
                     </span>
                 </span>
             </h1>
-            {/* 
             {collection.description && (
                 <Markdown className="mt-12 max-w-2xl text-pretty 2xl:max-w-5xl">
                     {collection.description}
                 </Markdown>
-            )} */}
+            )}
         </Container>
     );
 };
