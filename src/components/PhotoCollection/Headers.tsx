ea5223b0e5ee1c9a695c82e7b394e07c993a2738
Sean McEmerson <boredofmath+github@gmail.com>
2025-10-24 02:00:33 +0100

    feat: Update custom header layout

--- src/components/PhotoCollection/Headers.tsx
+++ src/components/PhotoCollection/Headers.tsx
@@ -32,24 +32,27 @@ const GreenHopHeader: React.FC<React.PropsWithChildren<Props>> = ({collection}) 
     const title2 = collection.pageTitle?.split(':')[1];
 
     return (
-        <Container className="pt-6 pb-10 sm:py-12 xl:py-20">
-            <h1 className="text-title-text overflow-hidden leading-none whitespace-nowrap uppercase underline-offset-4 group-hover:underline">
+        <Container className="animate-in fade-in slide-in-from-bottom-2 pt-6 pb-10 duration-500 sm:py-12 xl:py-20">
+            <h1 className="text-title-text overflow-hidden leading-none uppercase underline-offset-4 group-hover:underline sm:whitespace-nowrap">
                 <span
-                    className="block font-extrabold tracking-tight"
-                    style={{fontSize: 'clamp(2.8rem, 9.3vw, 9.3vw)'}}
+                    className="block font-extrabold tracking-tight text-pretty 2xl:!text-[7.3rem]"
+                    style={{fontSize: 'clamp(2.8rem, 7.9vw, 12vw)'}}
                 >
                     {title1}
                 </span>
-                <span className="mt-1 block" style={{fontSize: 'clamp(1rem, 4vw, 9vw)'}}>
+                <span className="mt-2 block" style={{fontSize: 'clamp(1.5rem, 3vw, 9vw)'}}>
                     {title2}
+                    <span className="sm:text-md ml-2 text-sm opacity-50 lg:text-lg">
+                        Autumn 2025
+                    </span>
                 </span>
             </h1>
-
+            {/* 
             {collection.description && (
                 <Markdown className="mt-12 max-w-2xl text-pretty 2xl:max-w-5xl">
                     {collection.description}
                 </Markdown>
-            )}
+            )} */}
         </Container>
     );
 };
