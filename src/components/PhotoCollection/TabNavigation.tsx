cb6744526f57c9d1f4f8a7759606695d5fdce54a
Sean McEmerson <boredofmath+github@gmail.com>
2025-10-27 18:31:20 +0000

    chore: UI tweaks

--- src/components/PhotoCollection/TabNavigation.tsx
+++ src/components/PhotoCollection/TabNavigation.tsx
@@ -15,14 +15,14 @@ const PhotoCollectionTabNavigation: React.FC<Props> = ({tabs}) => {
     const activeTheme = useAtomValue(activeThemeAtom);
 
     return (
-        <div className="bg-active-theme/90 top-site-header-scrolled sticky z-50 backdrop-blur-2xl transition-all duration-500 will-change-transform lg:-mx-8">
+        <div className="bg-active-theme/90 top-site-header-scrolled sticky z-50 -mx-4 overflow-x-auto whitespace-nowrap backdrop-blur-2xl transition-colors duration-500 will-change-transform md:mx-0">
             <Container className="mx-auto w-full max-w-[110rem] px-0 lg:px-0">
                 <ul className="flex w-full flex-row gap-1 py-1">
                     {tabs.map(tab => (
                         <li key={tab.id} className="flex-grow">
                             <Link
                                 className={clsx(
-                                    'block rounded-sm py-2 text-center text-sm font-medium uppercase underline-offset-2 transition-colors duration-200 ease-in-out',
+                                    'block rounded-sm py-2 text-center text-xs font-medium uppercase underline-offset-4 transition-colors duration-200 ease-in-out md:text-sm',
                                     {
                                         'bg-accent pointer-events-none underline':
                                             tab.id === activeTheme.id,
