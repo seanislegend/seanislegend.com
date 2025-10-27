e14ecde60206e12165d3c223a0cc37161da02121
Sean McEmerson <boredofmath+github@gmail.com>
2025-10-27 18:50:30 +0000

    fix merge

--- src/components/PhotoCollection/Blocks.tsx
+++ src/components/PhotoCollection/Blocks.tsx
@@ -154,7 +154,6 @@ const OnePortraitOneLandscapeMediumTwoLandscapeSmall: React.FC<PhotoBlockCompone
 const OnePortraitOneLandscapeMediumFourLandscapeSmall: React.FC<PhotoBlockComponent> = ({
     photos,
     renderPhoto,
-    reverse,
     reverse
 }) => (
     <Grid>
@@ -177,27 +176,7 @@ const OnePortraitOneLandscapeMediumFourLandscapeSmall: React.FC<PhotoBlockCompon
                 </>
             )}
         >
-    <<<<<<< HEAD
-        <Condition
-            condition={reverse}
-            fallbackWrapper={children => (
-                <>
-                    {children}
-                    <Column className="col-span-12 md:col-span-6">
-                        {renderPhoto(photos, 0, 6)}
-                    </Column>
-                </>
-            )}
-            wrapper={children => (
-                <>
-                    <Column className="col-span-12 md:col-span-6">
-                        {renderPhoto(photos, 0, 6)}
-                    </Column>
-                    {children}
-                </>
-            )}
-        >
-            <Column className="col-span-12 flex h-full flex-col justify-between gap-4 md:col-span-6">
+            <Column className="col-span-12 flex h-full flex-col justify-between gap-2 md:col-span-6 md:gap-4">
                 <Grid gridCols="grid-cols-12" hasTestId={false}>
                     <Column className="col-span-6">{renderPhoto(photos, 1, 6)}</Column>
                     <Column className="col-span-6">{renderPhoto(photos, 2, 6)}</Column>
@@ -211,22 +190,6 @@ const OnePortraitOneLandscapeMediumFourLandscapeSmall: React.FC<PhotoBlockCompon
                 </Grid>
             </Column>
         </Condition>
-=======
-        <Column className="col-span-12 flex h-full flex-col justify-between gap-2 md:col-span-6 md:gap-4">
-                <Grid gridCols="grid-cols-12" hasTestId={false}>
-                    <Column className="col-span-6">{renderPhoto(photos, 1, 6)}</Column>
-                    <Column className="col-span-6">{renderPhoto(photos, 2, 6)}</Column>
-                </Grid>
-                <Grid gridCols="grid-cols-12" hasTestId={false}>
-                    <Column className="col-span-12">{renderPhoto(photos, 3)}</Column>
-                </Grid>
-                <Grid gridCols="grid-cols-12" hasTestId={false}>
-                    <Column className="col-span-6">{renderPhoto(photos, 4, 6)}</Column>
-                    <Column className="col-span-6">{renderPhoto(photos, 5, 6)}</Column>
-                </Grid>
-            </Column>
-        </Condition>
->>>>>>> f2a1844 (feat: Reduce grid gaps on mobile)
     </Grid>
 );
 
