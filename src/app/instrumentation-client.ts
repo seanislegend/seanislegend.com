26f509eb1ad6ca8bfbc838ac6bbfc34df4b3bd6b
Sean McEmerson <boredofmath+github@gmail.com>
2025-11-04 16:56:54 +0000

    feat: Add Botid config

--- src/app/instrumentation-client.ts
+++ src/app/instrumentation-client.ts
@@ -1,5 +1,9 @@
 import {initBotId} from 'botid/client/core';
 
 initBotId({
+<<<<<<< HEAD
     protect: [{method: 'POST', path: '/green-hop-beer'}]
+=======
+    protect: [{method: 'POST', path: '/green-hop-beer/*'}]
+>>>>>>> 1033cec (feat: Add Botid config)
 });
