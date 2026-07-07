abb787e49df32112f765caee20549cb854359bba
Sean McEmerson <boredofmath+github@gmail.com>
2026-07-07 12:50:18 +0100

    chore: UI

--- src/components/SiteMenu/DynamicMenu/Categories.tsx
+++ src/components/SiteMenu/DynamicMenu/Categories.tsx
@@ -18,7 +18,7 @@ const DynamicMenuCategories: React.FC<Props> = ({tags}) => {
                     <CaretDownIcon className="h-3 w-3 fill-current transition-transform duration-300 ease-in-out group-data-popup-open:rotate-180" />
                 </span>
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="data-ending-style:data-activation-direction=left:translate-x-[50%] data-ending-style:data-activation-direction=right:translate-x-[-50%] data-starting-style:data-activation-direction=left:translate-x-[-50%] data-starting-style:data-activation-direction=right:translate-x-[50%] animate-menu-card-in h-full w-[250px] list-none px-4 pt-3 pb-7 transition-[opacity,transform,translate] duration-(--duration) ease-(--easing) data-ending-style:opacity-0">
+            <NavigationMenu.Content className="data-ending-style:data-activation-direction=left:translate-x-[50%] data-ending-style:data-activation-direction=right:translate-x-[-50%] data-starting-style:data-activation-direction=left:translate-x-[-50%] data-starting-style:data-activation-direction=right:translate-x-[50%] animate-menu-card-in h-full w-[250px] list-none px-4 py-4 transition-[opacity,transform,translate] duration-(--duration) ease-(--easing) data-ending-style:opacity-0">
                 <ul className="grid grid-cols-1 gap-y-2.5" data-testid="categories-grid">
                     {tags.map(tag => (
                         <li key={tag.slug} className="flex">
